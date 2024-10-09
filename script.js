77a0cf7675284045d3425c24ee808efd1b7fdd29
Prince-Shohel <shorifulislam9038@gmail.com>
2024-10-09 14:21:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Marging two arrays of data fetched from Separate APIs into a single list.
+
+let arr1 = ['name','akash','nirob','shohel','talha','anik']
+let arr2 = ['name','akash','nirob','shohel','talha','anik']
+
+let num = [...arr1, ...arr2]
